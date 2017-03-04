ad537a1665ea0d91b4e6604f1f78ffd494de03df
Phayao Boonon <phayaob@gmail.com>
2017-03-04 23:49:01 +0700

    Create the data model

---
+++ ContosoUniversity/src/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentData { get; set; }
+        
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
