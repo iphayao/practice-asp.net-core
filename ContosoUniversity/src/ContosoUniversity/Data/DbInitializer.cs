540a8d90adc7816850556c121806b7ae8a9b4e12
Phayao Boonon <phayaob@gmail.com>
2017-03-06 23:29:47 +0700

    Reading related data

--- ContosoUniversity/src/ContosoUniversity/Data/DbInitializer.cs
+++ ContosoUniversity/src/ContosoUniversity/Data/DbInitializer.cs
@@ -91,13 +91,27 @@ namespace ContosoUniversity.Data
 
             var courses = new Course[]
             {
-                new Course {CourseID = 1050, Title = "Chemistry", Credits = 3},
-                new Course {CourseID = 4022, Title = "Microeconomics", Credits = 3},
-                new Course {CourseID = 4041, Title = "Macroeconomics", Credits = 3},
-                new Course {CourseID = 1045, Title = "Calculus", Credits = 4},
-                new Course {CourseID = 3141, Title = "Trigonometry", Credits = 4},
-                new Course {CourseID = 2021, Title = "Composition", Credits = 3},
-                new Course {CourseID = 2042, Title = "Literature", Credits = 4}
+                new Course {CourseID = 1050, Title = "Chemistry",      Credits = 3,
+                    DepartmentID = departments.Single( s => s.Name == "Engineering").DepartmentID
+                },
+                new Course {CourseID = 4022, Title = "Microeconomics", Credits = 3,
+                    DepartmentID = departments.Single( s => s.Name == "Economics").DepartmentID
+                },
+                new Course {CourseID = 4041, Title = "Macroeconomics", Credits = 3,
+                    DepartmentID = departments.Single( s => s.Name == "Economics").DepartmentID
+                },
+                new Course {CourseID = 1045, Title = "Calculus",       Credits = 4,
+                    DepartmentID = departments.Single( s => s.Name == "Mathematics").DepartmentID
+                },
+                new Course {CourseID = 3141, Title = "Trigonometry",   Credits = 4,
+                    DepartmentID = departments.Single( s => s.Name == "Mathematics").DepartmentID
+                },
+                new Course {CourseID = 2021, Title = "Composition",    Credits = 3,
+                    DepartmentID = departments.Single( s => s.Name == "English").DepartmentID
+                },
+                new Course {CourseID = 2042, Title = "Literature",     Credits = 4,
+                    DepartmentID = departments.Single( s => s.Name == "English").DepartmentID
+                },
             };
 
             foreach(Course c in courses)
