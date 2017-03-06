faf68c506e148a8e3c324f773cf16bb6909e4911
Phayao Boonon <phayaob@gmail.com>
2017-03-06 16:36:43 +0700

    Create an About page that shows Student staistics

--- ContosoUniversity/src/ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/src/ContosoUniversity/Controllers/HomeController.cs
@@ -3,21 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;
+using ContosoUniversity.Models;
 
 namespace ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> About()
         {
-            ViewData["Message"] = "Your application description page.";
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentData into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
 
-            return View();
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Contact()
